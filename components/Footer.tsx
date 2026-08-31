a17a3fb7af3634d1e4ab26b4a0b3cf6ff68e571b
abhishekemavens-glitch <abhishekemavens@gmail.com>
2026-08-31 17:51:45 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,6 +68,7 @@ export default function Footer() {
                   nodes {
                     id
                     title
+                    mainLogo
                     description
                     linkedin
                     twitter
