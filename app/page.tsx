d5e15148d735206f4f7a46218204a88950405a67
abhishekemavens-glitch <abhishekemavens@gmail.com>
2026-08-31 13:26:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,6 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Industries from "../components/Industries";
 import WhyCodm from "../components/WhyCodm";
-import ServicesSection from "../components/ServicesSection";
-import TrustedBy from "../components/TrustedBy";
-import Testimonials from "../components/Testimonials";
-import LatestBlogs from "../components/LatestBlogs";
 
 export default function Home() {
   return (
@@ -14,10 +10,6 @@ export default function Home() {
       <Hero />
       <Industries />
       <WhyCodm />
-      <ServicesSection />
-      <TrustedBy />
-      <Testimonials />
-      <LatestBlogs />
     </>
   );
 }
